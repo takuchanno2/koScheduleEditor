d7484c6619c7453c179480a5302021cd79975609
takuchan No.2 <taku_yosi@hotmail.com>
2014-07-03 23:46:16 +0900

    Initial Commit.

---
+++ koScheduleEditor/Task.ts
@@ -0,0 +1,63 @@
+﻿/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="Scripts/typings/underscore/underscore.d.ts" />
+/// <reference path="Scripts/typings/knockout/knockout.d.ts" />
+/// <reference path="Scripts/typings/knockout.es5/knockout.es5.d.ts" />
+
+class TaskViewModel {
+    public constructor(public name: String = "") {
+        ko.track(this);
+    }
+
+    public clone(): TaskViewModel {
+        return new TaskViewModel(this.name);
+    }
+
+    public copyFrom(task: TaskViewModel) {
+        this.name = task.name;
+    }
+}
+
+class TaskListViewModel {
+    public tasks: TaskViewModel[] = [];
+    private focusedTask = new TaskViewModel();
+    public focusedTaskOriginal: TaskViewModel = null;
+
+    public constructor() {
+        this.focus(null);
+        ko.track(this);
+    }
+
+    public add = () => {
+        if (!this.isEditingTask) { this.tasks.push(this.focusedTask); }
+        this.focus(null);
+    };
+
+    public cancel = () => {
+        if (this.isEditingTask) {
+            this.focusedTask.copyFrom(this.focusedTaskOriginal);
+        }
+
+        this.focus(null);
+    };
+
+    public remove = (task: TaskViewModel) => {
+        if (task === this.focusedTask) {
+            this.focusedTaskOriginal = null;
+        }
+        this.tasks.remove(task);
+    };
+
+    public focus = (task: TaskViewModel) => {
+        if (task) {
+            this.focusedTask = task;
+            this.focusedTaskOriginal = task.clone();
+        } else {
+            this.focusedTask = new TaskViewModel();
+            this.focusedTaskOriginal = null;
+        }
+    };
+
+    public get isEditingTask() {
+        return (this.focusedTaskOriginal !== null);
+    }
+}
---
+++ koScheduleEditor/app.ts
@@ -0,0 +1,7 @@
+﻿/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="Scripts/typings/knockout/knockout.d.ts" />
+/// <reference path="Scripts/typings/knockout.es5/knockout.es5.d.ts" />
+
+$(() => {
+    ko.applyBindings(new TaskListViewModel());
+});
