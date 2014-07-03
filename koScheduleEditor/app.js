ef8643b36d52b1ddcc2df1e80ff2954196c7ae4e
takuchan No.2 <taku_yosi@hotmail.com>
2014-07-04 00:08:12 +0900

    余計なファイルを削除

--- koScheduleEditor/app.js
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
-/// <reference path="Scripts/typings/knockout/knockout.d.ts" />
-/// <reference path="Scripts/typings/knockout.es5/knockout.es5.d.ts" />
-$(function () {
-    ko.applyBindings(new TaskListViewModel());
-});
-//# sourceMappingURL=app.js.map
